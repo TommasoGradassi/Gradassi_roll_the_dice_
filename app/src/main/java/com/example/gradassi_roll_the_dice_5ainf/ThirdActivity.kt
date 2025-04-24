b60fd9f92ecf4357e9b7863a5878adf0ca7add50
tommasogradassi <tommasogradassi811@gmail.com>
2025-04-24 09:37:53 +0200

    Aggiunta foto vittoria o sconfitta

--- app/src/main/java/com/example/gradassi_roll_the_dice_5ainf/ThirdActivity.kt
+++ app/src/main/java/com/example/gradassi_roll_the_dice_5ainf/ThirdActivity.kt
@@ -2,6 +2,7 @@ package com.example.gradassi_roll_the_dice_5ainf
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,20 +16,25 @@ class ThirdActivity : AppCompatActivity() {
         val random1 = (1..2).random()
         val random2 = (1..2).random()
         var n = intent.getIntExtra("Random", -1)
-        var somma=random1+random2
+        var somma = random1 + random2
         Log.d("DEBUG", "Numero uscito: $n")
         Log.d("DEBUG", "Numeri sommati: $random1 + $random2 = $somma")
-        var Stringa1=""
+        var Stringa1 = ""
+        var foto : Int
         var risultato = if (n > somma) {
 
             Stringa1 = "Hai vinto!"
+            foto =R.drawable.victory_1
         } else {
-            Stringa1="Hai perso!"
+            Stringa1 = "Hai perso!"
+            foto=R.drawable.loser_1
         }
 
-        val textView3=findViewById<TextView>(R.id.textView_Estrazione)
-        textView3.text="$n";
+        var image_victory_lose= findViewById<ImageView>(R.id.Image_victory_lose)
+        image_victory_lose.setImageResource(foto)
+        val textView3 = findViewById<TextView>(R.id.textView_Estrazione)
+        textView3.text = "$n";
         val textView2 = findViewById<TextView>(R.id.Hai_perso_vinto)
-        textView2.text=Stringa1;
+        textView2.text = Stringa1;
     }
 }
